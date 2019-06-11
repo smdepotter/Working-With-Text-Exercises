fd81e255207a2ff925aea59781b1b84af7abd17c
Sean DePotter <seandep@live.com>
2019-06-11 03:40:20 -0400

    Exercise 2

--- Working With Text Exercises/Program.cs
+++ Working With Text Exercises/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text;
 
 namespace Working_With_Text_Exercises
 {
@@ -8,7 +10,7 @@ namespace Working_With_Text_Exercises
     {
         static void Main(string[] args)
         {
-            Exercise1();
+            Exercise2();
         }
 
         static void Exercise1()
@@ -39,7 +41,52 @@ namespace Working_With_Text_Exercises
 
         static void Exercise2()
         {
+            /*Write a program and ask the user to enter a few numbers separated by a hyphen.
+             If the user simply presses Enter, without supplying an input, exit immediately;
+             otherwise, check to see if there are duplicates. If so, display "Duplicate"
+             on the console.
+             */
 
+            Console.WriteLine("Enter a few numbers. Separate with hyphen");
+
+            var userInput = Console.ReadLine();
+            var numbers = userInput.Split("-").Select(n => Convert.ToInt32(n)).ToList();
+            numbers.Sort();
+
+            if (String.IsNullOrWhiteSpace(userInput)) Environment.Exit(1);
+
+            var repeatedNumbers = new List<int>();
+            var repeatedNumbersStringBuilder = new StringBuilder();
+
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] == numbers[i-1])
+                {
+                    //added func; shows user repeated numbers
+                    if (!repeatedNumbers.Contains(numbers[i]))
+                    {
+                        repeatedNumbers.Add(numbers[i]);
+                    }
+                }
+            }
+
+            if (repeatedNumbers.Count() != 0)
+            {
+                foreach (var number in repeatedNumbers)
+                {
+                    repeatedNumbersStringBuilder.Append(number + ",");
+
+                }
+
+                Console.WriteLine("Duplicates Found: " + repeatedNumbersStringBuilder);
+            }
+            else
+            {
+                Console.WriteLine("No Duplicates");
+            }
+
+            
         }
     }
 }

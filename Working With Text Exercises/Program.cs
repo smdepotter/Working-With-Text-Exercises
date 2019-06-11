617406e69d4dbf67ba3e76b3858de28a613efab4
Sean DePotter <seandep@live.com>
2019-06-11 03:48:28 -0400

    Exercise 3

--- Working With Text Exercises/Program.cs
+++ Working With Text Exercises/Program.cs
@@ -10,7 +10,7 @@ namespace Working_With_Text_Exercises
     {
         static void Main(string[] args)
         {
-            Exercise2();
+            Exercise3();
         }
 
         static void Exercise1()
@@ -87,6 +87,26 @@ namespace Working_With_Text_Exercises
             }
 
             
+        }
+
+        static void Exercise3()
+        {
+            /*Write a program and ask the user to enter a time value in the 24-hour
+             time format
+             (e.g. 19:00). A valid time should be between 00:00 and 23:59.
+             If the time is valid, display "Ok"; otherwise, display "Invalid Time". 
+             If the user doesn't provide any values, consider it as invalid time.
+             */
+
+            Console.WriteLine("Enter a time");
+
+            TimeSpan span;
+            if (TimeSpan.TryParse(Console.ReadLine(), out span)) Console.WriteLine("Ok");
+            else
+            {
+                Console.WriteLine("Invalid Time");
+            }
+
         }
     }
 }
